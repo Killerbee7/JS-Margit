301671f488095782d57c2fa25ec4534e9ad57e67
student <student@BC-minimac029s-Mac-mini.local>
2022-08-17 14:54:36 +0300

    working

--- week2/task4/main.js
+++ week2/task4/main.js
@@ -1,20 +1,25 @@
 function checkRandomnum(){
 
-const a = prompt("enter first number");
-const b = prompt("enter second numbe");
-const c = prompt("enter third number");
+const a =Number(prompt("enter first number"));
+const b =Number(prompt("enter second numbe"));
+const c =Number(prompt("enter third number"));
 
 let sum, mult;
 
 if (a >= 0 && b >= 0  && c >= 0 )   {
-   sum = a+b+c;
+   sum = a+b+c
    mult = a*b*c;
    return console.log("sum is:",sum, "and multiplication is:", mult);
 }
 
-else {
-    return console.log("sorry");
+else if (a >= 0 || b >= 0  || c >= 0 ) {
+    sum  = a+b+c
+    return console.log("sum is;", sum);
 }
+
+else (a < 0 && b < 0  && c < 0) 
+    return console.log("Only negatives");
+
 };
 
 checkRandomnum()
